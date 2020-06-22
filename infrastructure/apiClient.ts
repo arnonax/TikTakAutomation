041831bfa9b3fbb9fdc4e90645f09d26b8ca72bb
arnonax <arnonaxelrod@hotmail.com>
2020-06-22 17:52:38 +0300

    Refactoring: added apiClient class

---
+++ infrastructure/apiClient.ts
@@ -0,0 +1,55 @@
+import { Logger } from "./logger";
+import Axios, { AxiosResponse } from "axios";
+
+export interface ITikTakResponseMessage {
+	error?: Error;
+}
+
+export class ApiClient {
+	private _baseUrl: string;
+	private _apiKey: string;
+
+	constructor(baseUrl: string, apiKey: string) {
+		this._baseUrl = baseUrl;
+		this._apiKey = apiKey;
+	}
+
+	async get<TResponse>(
+		path: string,
+		queryStringArgs?: any
+	): Promise<TResponse> {
+		let queryString = "";
+		if (queryStringArgs) {
+			queryString = Object.keys(queryStringArgs)
+				.map((key) => `${key}=${queryStringArgs[key]}`)
+				.join("&");
+			if (queryString.length > 0) queryString = "?" + queryString;
+		}
+		const fullUrl = `${this._baseUrl}${path}${queryString}`;
+
+		Logger.logMessage(`Sending request to ${fullUrl}`);
+
+		const response = await Axios.get<TResponse>(fullUrl, {
+			headers: { "x-api-key": this._apiKey },
+		});
+
+		this.validateResponse(response);
+		Logger.logMessage(`Response=${JSON.stringify(response.data)}`);
+
+		return response.data;
+	}
+
+	private validateResponse<T extends ITikTakResponseMessage>(
+		response: AxiosResponse<T>
+	) {
+		const statusCode = response.status;
+		if (statusCode === undefined || statusCode >= 300)
+			throw new Error(
+				`Invalid status received from server: ${statusCode}`
+			);
+
+		const error = response.data.error;
+		if (JSON.stringify(error) != "{}")
+			throw new Error(`Received error from server: ${error}`);
+	}
+}
--- tikTak/TikTakApi.ts
+++ tikTak/TikTakApi.ts
@@ -1,15 +1,12 @@
 import { TikTakSearchResults } from "./TikTakSearchResults";
-import Axios, { AxiosResponse } from "axios";
+import Axios from "axios";
 import {
 	RouteOptionsResponse,
 	TravelOptionState,
 } from "../typescript-node-client/api";
 import "../infrastructure/logger";
 import { Logger } from "../infrastructure/logger";
-
-interface ITikTakResponseMessage {
-	error?: Error;
-}
+import { ApiClient } from "../infrastructure/apiClient";
 
 // TODO: fix swagger to define the reponse of TravelOptions correctly.
 type TravelOptionsResponse = {
@@ -18,69 +15,34 @@ type TravelOptionsResponse = {
 };
 
 export class TikTakApi {
-	private _baseUrl: string;
-	private _apiKey: string;
+	private _apiClient: ApiClient;
 
 	constructor(baseUrl: string, apiKey: string) {
-		this._baseUrl = baseUrl;
-		this._apiKey = apiKey;
+		this._apiClient = new ApiClient(baseUrl, apiKey);
 	}
 
 	async getTravelOptions(
 		origin: string,
 		destination: string
 	): Promise<TikTakSearchResults> {
-		Logger.logMessage(
-			`Sending RouteOptions request from ${origin} to ${destination}`
-		);
+		const routeOptionsResponse = await this._apiClient.get<
+			RouteOptionsResponse
+		>("routes/options", {
+			origin: origin,
+			destination: destination,
+			transit_mode: "bus",
+		});
 
-		const routeOptionsResponse = await Axios.get<RouteOptionsResponse>(
-			`${this._baseUrl}routes/options?origin=${origin}&destination=${destination}&transit_mode=bus`,
-			{ headers: { "x-api-key": this._apiKey } }
-		);
-
-		this.validateResponse(routeOptionsResponse);
-		Logger.logMessage(
-			`Got RouteOptions response: ${JSON.stringify(
-				routeOptionsResponse.data
-			)}`
-		);
-
-		const requestId = routeOptionsResponse.data.data?.requestId;
+		const requestId = routeOptionsResponse.data?.requestId;
 		if (requestId === undefined)
 			throw new Error(
 				"requestId is not defined in route options response"
 			);
 
-		Logger.logMessage(
-			`Sending TravelOptions request with requestId ${requestId}`
-		);
-		const travelOptionsResponse = await Axios.get<TravelOptionsResponse>(
-			`${this._baseUrl}travel-options/${requestId}`,
-			{ headers: { "x-api-key": this._apiKey } }
-		);
+		const travelOptionsResponse = await this._apiClient.get<
+			TravelOptionsResponse
+		>(`travel-options/${requestId}`);
 
-		this.validateResponse(travelOptionsResponse);
-		Logger.logMessage(
-			`Got TravelOptions response: ${JSON.stringify(
-				travelOptionsResponse.data
-			)}`
-		);
-
-		return new TikTakSearchResults(travelOptionsResponse.data.data);
-	}
-
-	validateResponse<T extends ITikTakResponseMessage>(
-		response: AxiosResponse<T>
-	) {
-		const statusCode = response.status;
-		if (statusCode === undefined || statusCode >= 300)
-			throw new Error(
-				`Invalid status received from server: ${statusCode}`
-			);
-
-		const error = response.data.error;
-		if (JSON.stringify(error) != "{}")
-			throw new Error(`Received error from server: ${error}`);
+		return new TikTakSearchResults(travelOptionsResponse.data);
 	}
 }
